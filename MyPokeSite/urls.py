172f0927055dd9ecfe28962b778aebd74e6d18bb
tsteele1432 <tsteele842@gmail.com>
2021-12-13 10:44:26 -0700

    Update urls.py

--- MyPokeSite/urls.py
+++ MyPokeSite/urls.py
@@ -18,6 +18,9 @@ from django.urls import path, include
 from . import settings
 from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
