8dce82d3bcab2524c88f738e60758c4f4a3669f2
oslunt <77417770+oslunt@users.noreply.github.com>
2021-12-13 10:43:58 -0700

    Updated update

--- homepages/views.py
+++ homepages/views.py
@@ -1,6 +1,6 @@
 from django.db import connection
 from django.shortcuts import render
-from homepages.models import PokemonInfo, PokemonType, Pokemon
+from homepages.models import PokemonInfo, PokemonType
 # Create your views here.
 
 def indexPageView(request):
@@ -35,10 +35,22 @@ def showPokemonPageView(request):
     return render(request, 'homepages/showPokemon.html', context)
 
 def showSinglePokemonPageView(request, pokemon_id):
-    data = Pokemon.objects.get(pokemon_id = pokemon_id)
-
+    data = PokemonInfo.objects.get(pokemon_id = pokemon_id)
+    cursor = connection.cursor()
+    query = "SELECT type FROM pokemon_type WHERE pokemon_id=%s"
+    cursor.execute(query, [pokemon_id])
+    columns = [col[0] for col in cursor.description]
+    type = [dict(zip(columns, row)) for row in cursor.fetchall()]
+    if len(type) < 2:
+        type1 = type[0]['type']
+        type2 = 'None'
+    else :
+        type1 = type[0]['type']
+        type2 = type[1]['type']
     context = {
-        "record": data
+        "record": data,
+        "type1": type1,
+        "type2": type2,
     }
 
     return render(request, 'homepages/editPokemon.html', context)
@@ -47,7 +59,43 @@ def updatePokemonPageView(request):
     if request.method == "POST":
         pokemon_id = request.POST['pokemon_id']
 
-        pokemon = Pokemon.objects.get(pokemon_id=pokemon_id)
+        pokemon = PokemonInfo.objects.get(pokemon_id=pokemon_id)
+        type1 = request.POST['type1']
+        type2 = request.POST['type2']
+        print(type2)
+        cursor = connection.cursor()
+        query = "SELECT type FROM pokemon_type WHERE pokemon_id=%s"
+        cursor.execute(query, [pokemon_id])
+        columns = [col[0] for col in cursor.description]
+        type = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        if type2 == 'None':
+            if len(type) < 2:
+                cursor = connection.cursor()
+                query = "UPDATE pokemon_type SET type=%s WHERE pokemon_id=%s"
+                cursor.execute(query, (type1, pokemon_id))
+            else:
+                cursor = connection.cursor()
+                query = "DELETE FROM pokemon_type WHERE pokemon_id=%s and type=%s"
+                cursor.execute(query, (pokemon_id, type[1]['type']))
+                cursor = connection.cursor()
+                query = "UPDATE pokemon_type SET type=%s WHERE pokemon_id=%s and type=%s"
+                cursor.execute(query, (type1, pokemon_id, type[0]['type']))
+        else:
+            if len(type) < 2:
+                cursor = connection.cursor()
+                query = "UPDATE pokemon_type SET type=%s WHERE pokemon_id=%s"
+                cursor.execute(query, (type1, pokemon_id))
+                cursor = connection.cursor()
+                query = "INSERT INTO pokemon_type(pokemon_id, type) VALUES(%s, %s)"
+                cursor.execute(query, (pokemon_id, type2))
+            else:
+                cursor = connection.cursor()
+                query = "UPDATE pokemon_type SET type=%s WHERE pokemon_id=%s and type=%s"
+                cursor.execute(query, (type1, pokemon_id, type[0]['type']))
+                cursor = connection.cursor()
+                query = "UPDATE pokemon_type SET type=%s WHERE pokemon_id=%s and type=%s"
+                cursor.execute(query, (type2, pokemon_id, type[1]['type']))
+                print('2 types')
 
         pokemon.name = request.POST['name']
         pokemon.image_path = request.POST['image_path']
@@ -60,7 +108,7 @@ def updatePokemonPageView(request):
     return showPokemonPageView(request)
 
 def deletePokemonPageView(request, pokemon_id):
-    data = Pokemon.objects.get(pokemon_id = pokemon_id)
+    data = PokemonInfo.objects.get(pokemon_id = pokemon_id)
     data.delete()
     return showPokemonPageView(request)
 
